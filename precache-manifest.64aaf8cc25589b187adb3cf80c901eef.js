026e6e0212691d3391c5e0d4df525c8f34ade9a7
AlexisRumbo <lrumboo@gmail.com>
2020-09-11 20:40:51 -0300

    Updates

---
+++ precache-manifest.64aaf8cc25589b187adb3cf80c901eef.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cd14f8ae840994075e683b0a9115c3",
+    "url": "/portfolio-ale-rumbo/index.html"
+  },
+  {
+    "revision": "1cbcd1836df5e7658c35",
+    "url": "/portfolio-ale-rumbo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6f9fad5258cdd073f0d6",
+    "url": "/portfolio-ale-rumbo/static/css/main.9e883fa8.chunk.css"
+  },
+  {
+    "revision": "1cbcd1836df5e7658c35",
+    "url": "/portfolio-ale-rumbo/static/js/2.c69deb9b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-ale-rumbo/static/js/2.c69deb9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9fad5258cdd073f0d6",
+    "url": "/portfolio-ale-rumbo/static/js/main.3d2838b2.chunk.js"
+  },
+  {
+    "revision": "8d276c26aa4aa315cb1c",
+    "url": "/portfolio-ale-rumbo/static/js/runtime-main.5fd9dd54.js"
+  },
+  {
+    "revision": "66982f21f84bd711679662997c296387",
+    "url": "/portfolio-ale-rumbo/static/media/banner.66982f21.jpg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolio-ale-rumbo/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c1e769605e4686003967900cb433dc65",
+    "url": "/portfolio-ale-rumbo/static/media/facebook.c1e76960.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/portfolio-ale-rumbo/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "780ae514ceb63bd46298a05b5d5c9876",
+    "url": "/portfolio-ale-rumbo/static/media/imgProgramacion.780ae514.png"
+  },
+  {
+    "revision": "46ce74c2e7da57b99857891a9e0ebf94",
+    "url": "/portfolio-ale-rumbo/static/media/instagram.46ce74c2.svg"
+  },
+  {
+    "revision": "1fd04ed64f73705b7add03cfa0059537",
+    "url": "/portfolio-ale-rumbo/static/media/linkedin.1fd04ed6.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio-ale-rumbo/static/media/logoPie.4009eef3.svg"
+  }
+]);
